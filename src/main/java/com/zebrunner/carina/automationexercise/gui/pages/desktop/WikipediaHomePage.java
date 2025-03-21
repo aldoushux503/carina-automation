287c1572ba1541b40416fdee2b293bca78912694
aldoushux503 <elnikovnikita4@gmail.com>
2025-03-21 23:14:01 +0100

    Refactor localisation test cases

--- src/main/java/com/zebrunner/carina/automationexercise/gui/pages/desktop/WikipediaHomePage.java
+++ src/main/java/com/zebrunner/carina/automationexercise/gui/pages/desktop/WikipediaHomePage.java
@@ -6,17 +6,52 @@ import com.zebrunner.carina.webdriver.config.WebDriverConfiguration;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.Locale;
 
 public class WikipediaHomePage extends WikipediaHomePageBase {
 
+    @FindBy(xpath = "//div[@id='js-lang-lists']//a")
+    private List<ExtendedWebElement> langList;
 
+    @FindBy(id = "js-lang-list-button")
+    private ExtendedWebElement langListBtn;
+
+    public WikipediaHomePage(WebDriver driver) {
+        super(driver);
+        setPageAbsoluteURL("https://www.wikipedia.org/");
+        setPageOpeningStrategy(PageOpeningStrategy.BY_URL);
+    }
+
+    public WikipediaLocalePage goToWikipediaLocalePage(WebDriver driver) {
+        openLangList();
+        if (!langList.isEmpty()) {
+            for (ExtendedWebElement languageBtn : langList) {
+                String localeStr = Configuration.getRequired(WebDriverConfiguration.Parameter.LOCALE);
+                Locale locale = parseLocale(localeStr);
+                if (languageBtn.getAttribute("lang").equals(locale.getLanguage())) {
+                    languageBtn.click();
+                    return new WikipediaLocalePage(driver);
+                }
+            }
+        }
+        throw new RuntimeException("No language ref was found");
+    }
+
+    public void openLangList() {
+        langListBtn.clickIfPresent();
+    }
+
+    private Locale parseLocale(String localeToParse) {
+        String[] localeSetttings = localeToParse.trim().split("_");
+        String lang, country = "";
+        lang = localeSetttings[0];
+        if (localeSetttings.length > 1) {
+            country = localeSetttings[1];
+        }
+        return new Locale(lang, country);
+    }
 }
--- src/test/java/com/zebrunner/carina/automationexercise/web/WikipediaLocalizationTest.java
+++ src/test/java/com/zebrunner/carina/automationexercise/web/WikipediaLocalizationTest.java
@@ -1,14 +1,110 @@
 package com.zebrunner.carina.automationexercise.web;
 
-
+import com.zebrunner.carina.automationexercise.gui.pages.desktop.WikipediaHomePage;
+import com.zebrunner.carina.automationexercise.gui.pages.desktop.WikipediaLocalePage;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import com.zebrunner.carina.utils.R;
+import com.zebrunner.carina.utils.resources.L10N;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-
-
-/**
- * Test for Wikipedia localization to verify content in different languages
- */
 public class WikipediaLocalizationTest implements IAbstractTest {
 
+    @BeforeMethod
+    public void setUpLocalization() {
+        // Enable localization testing
+        R.CONFIG.put("localization_testing", "true");
+    }
+
+    @Test(dataProvider = "languages")
+    public void testMainPageTitleLocalization(String locale, String language) {
+        R.CONFIG.put("locale", locale, true);
+        L10N.setLocale(locale);
+        L10N.load();
+
+        WikipediaHomePage homePage = new WikipediaHomePage(getDriver());
+        homePage.open();
+
+        WikipediaLocalePage localePage = homePage.goToWikipediaLocalePage(getDriver());
+
+        String expectedTitle =  L10N.getText("LocalePage.mainPageTitle");
+        String actualTitle = localePage.getPageTitle();
+
+        Assert.assertEquals(actualTitle, expectedTitle,
+                "Main page title is not correctly localized in " + language + ". Expected: " + expectedTitle + ", Actual: " + actualTitle);
+    }
+
+    @Test(dataProvider = "languages")
+    public void testNavigationElementsLocalization(String locale, String language) {
+        WikipediaHomePage homePage = new WikipediaHomePage(getDriver());
+        homePage.open();
+
+        R.CONFIG.put("locale", locale);
+        WikipediaLocalePage localePage = homePage.goToWikipediaLocalePage(getDriver());
+        Assert.assertTrue(localePage.isMainPageLinkTextEqualTo(R.TESTDATA.get("LocalePage.mainPageLink")),
+                "Main page link text is not correctly localized in " + language);
+        Assert.assertTrue(localePage.isRandomPageLinkTextEqualTo(R.TESTDATA.get("LocalePage.randomPageLink")),
+                "Random page link text is not correctly localized in " + language);
+    }
+
+    @Test(dataProvider = "languages")
+    public void testSearchFunctionalityLocalization(String locale, String language) {
+        R.CONFIG.put("locale", locale, true);
+        L10N.setLocale(locale);
+        L10N.load();
+        WikipediaHomePage homePage = new WikipediaHomePage(getDriver());
+        homePage.open();
+
+
+        WikipediaLocalePage localePage = homePage.goToWikipediaLocalePage(getDriver());
+
+        String expectedPlaceholder = L10N.getText("LocalePage.searchPlaceholder");
+        String actualPlaceholder = localePage.getSearchPlaceholder();
+
+        Assert.assertEquals(actualPlaceholder, expectedPlaceholder,
+                "Search placeholder is not correctly localized in " + language + ". Expected: " + expectedPlaceholder + ", Actual: " + actualPlaceholder);
+    }
+
+    @Test(dataProvider = "languages")
+    public void testAccountLinksLocalization(String locale, String language) {
+        WikipediaHomePage homePage = new WikipediaHomePage(getDriver());
+        homePage.open();
+
+        R.CONFIG.put("locale", locale);
+        WikipediaLocalePage localePage = homePage.goToWikipediaLocalePage(getDriver());
+        Assert.assertTrue(localePage.isLoginLinkTextEqualTo(R.TESTDATA.get("LocalePage.loginLink")),
+                "Login link text is not correctly localized in " + language);
+        Assert.assertTrue(localePage.isCreateAccountLinkTextEqualTo(R.TESTDATA.get("LocalePage.createAccountLink")),
+                "Create account link text is not correctly localized in " + language);
+    }
+
+    @Test(dataProvider = "languages")
+    public void testLanguageSwitching(String locale, String language) {
+        WikipediaHomePage homePage = new WikipediaHomePage(getDriver());
+        homePage.open();
+
+        R.CONFIG.put("locale", locale);
+        WikipediaLocalePage localePage = homePage.goToWikipediaLocalePage(getDriver());
+
+        boolean correctLanguage = localePage.isCurrentLanguage(language);
+        Assert.assertTrue(correctLanguage,
+                "Language was not switched correctly to " + language + ". Current URL: " + getDriver().getCurrentUrl());
+
+        String langAttribute = localePage.getLangAttribute();
+        String expectedLangCode = locale.substring(0, 2).toLowerCase();
+        Assert.assertEquals(langAttribute, expectedLangCode,
+                "HTML lang attribute doesn't match expected language. Expected: " + expectedLangCode + ", Actual: " + langAttribute);
+    }
+
+    @DataProvider(name = "languages")
+    public Object[][] provideLanguages() {
+        return new Object[][] {
+                {"en_US", "English"},
+                {"fr_FR", "Français"},
+                {"es_ES", "Español"}
+        };
+    }
 }
