2647a16e79760c9e09d5715b8ecdee85824902fa
aldoushux503 <elnikovnikita4@gmail.com>
2025-03-21 20:34:57 +0100

    Refactor localisation test cases classes

--- src/main/java/com/zebrunner/carina/automationexercise/gui/pages/common/WikipediaHomePageBase.java
+++ src/main/java/com/zebrunner/carina/automationexercise/gui/pages/common/WikipediaHomePageBase.java
@@ -10,35 +10,5 @@ public abstract class WikipediaHomePageBase extends AbstractPage {
         super(driver);
     }
 
-    public abstract String getSearchPlaceholder();
 
-    public abstract boolean isLogoPresent();
-
-    public abstract String getLogoText();
-
-    public abstract String getMainPageLinkText();
-
-    public abstract String getRandomPageLinkText();
-
-    public abstract String getLoginLinkText();
-
-    public abstract String getCreateAccountLinkText();
-
-    public abstract String getSearchButtonText();
-
-    public abstract String getFooterText();
-
-    public abstract void clickMoreButton();
-
-    public abstract void hoverContribElem();
-
-    public abstract void clickDiscussionBtn();
-
-    public abstract String getDiscussionText();
-
-    public abstract void hoverCreateAccountElem();
-
-    public abstract void hoverWelcomeText();
-
-    public abstract void selectLanguage(String language);
 }
